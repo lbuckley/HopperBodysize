0c08a5855a5b293b0c0bf9f91742f22ce097598c
Lauren Buckley <lbbuckley@gmail.com>
2024-03-07 21:27:30 -0800

    add repro plot

--- BodysizeAdditionalAnalyses.R
+++ BodysizeAdditionalAnalyses.R
@@ -1,5 +1,8 @@
 library(ggplot2)
 library(reshape)
+library(reshape2)
+library(nlme)
+library(lme4)
 
 #Compare wet and dry size
 
@@ -22,8 +25,18 @@ mod1=lm(Femur.dry~0 + Femur.fresh, data=wdw)
 setwd("/Volumes/GoogleDrive/Shared drives/RoL_FitnessConstraints/projects/BodySize/data/AdditionalData/")
 repro= read.csv("Levy_Female Gradient Data Grasshopper.csv")
 
-ggplot(data=repro, aes(x=Femur_mm, y = ClutchMass_g, color=factor(Elevation_m))) + 
-  geom_point()+facet_wrap(Species~., scales="free")+geom_smooth(method="lm")+theme_bw()
+#model
+rvars<- c("EggMass","NOvarioles","NFunctOvarioles","PropFunctOvarioles","ClutchMass_g")
+y<- rvars[5]
+mod.lmer <- lm(EggMass ~Femur_mm*Elevation_m*Species,
+                 na.action = 'na.omit', data = repro)
+#plot_model(mod.lmer, type = "pred", terms = c("Femur_mm","Species"), show.data=TRUE)
+plot_model(mod.lmer, type = "pred", terms = c("Femur_mm","Elevation_m", "Species"), show.data=TRUE)
+
+repro.l<- melt(repro, id.vars = c("ID","Species","Site","Femur_mm","Elevation_m"))
+
+ggplot(data=repro.l, aes(y=value, x = Femur_mm, color=factor(Elevation_m))) + 
+  geom_point()+facet_grid(variable~Species, scales="free")+geom_smooth(method="lm")+theme_bw()
 
 specs= c("A. clavatus","M. boulderensis","C. pellucida","M. sanguinipes") 
 mod1=lm(ClutchMass_g~Femur_mm + factor(Elevation_m), data=repro[repro$Species==specs[4],])
